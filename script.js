ea0ff39971d6d097f90146dc71d7bdc0c14539dc
parangat11 <parangataditya001@gmail.com>
2024-10-01 22:51:44 +0530

    Add some CSS to the buttons.

--- script.js
+++ script.js
@@ -23,11 +23,17 @@ function modulo(firstNumber, secondNumber) {
 let firstNumber = undefined, secondNumber = undefined, operator = undefined;
 let displayValue = '';
 const display = document.querySelector('.display');
-const calci = document.querySelector('calculator');
+const calci = document.querySelector('.calculator');
 
 calci.addEventListener('click', (e) => {
     display.textContent = displayValue;
 });
+
+// document.addEventListener('DOMContentLoaded', (e) => {
+//     const decimal = document.querySelector('.decimal');
+//     const num0 = document.querySelector('.num-0');
+//     decimal.style.width = 2 * num0.style.width;
+// })
 
 function operate(firstNumber, secondNumber, operator) {
     switch(operate) {
